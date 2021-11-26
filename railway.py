8556c813c52a585ff2e38f55584e5a77ad05db15
asimgeek <asim2018off@gmail.com>
2021-11-27 00:22:11 +0530

    Homepage_draft2

--- railway.py
+++ railway.py
@@ -21,8 +21,6 @@ MDTextField:
 passw = """
 MDTextField:
     hint_text: "Enter Password"
-    helper_text: "Forgot Password"
-    helper_text_mode: "on_focus"
     icon_right_color: app.theme_cls.primary_color
     pos_hint:{'center_x': 0.6, 'center_y': 0.5}
     size_hint_x:None
