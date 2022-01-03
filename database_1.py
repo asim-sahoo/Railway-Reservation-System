74274e71cfe55309511ea99273a69e3fc633e110
SagnikBarik <b.sgnk2003@gmail.com>
2022-01-03 19:55:46 +0530

    new sunlight 2

--- database_1.py
+++ database_1.py
@@ -1,5 +1,3 @@
-import datetime
-
 class DataBase1:
     def __init__(self, filename):
         self.filename = filename
@@ -7,11 +5,11 @@ class DataBase1:
         self.file = None
 
     def add_train(self, f_st, t_st, date, month, day):
-        self.train = [f_st.strip(), t_st.strip(), date.strip(), month.strip(), day.strip()]
+        self.train = [f_st,";", t_st,";", date,";", month,";", day]
         self.save()
         return 1
 
     def save(self):
         with open(self.filename, "w") as f:
-            for i in self.train:
-                f.write(i)
+            for i in range(9):
+                f.write(self.train[i])
